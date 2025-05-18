66a148baaba220eb59ed817d823fcfecd707bb30
dotrong-code <dotrong159357@gmail.com>
2025-05-18 23:49:21 +0700

    update_historyTable

---
+++ BE/GRRWS.Domain/Entities/DeviceErrorHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRRWS.Domain.Entities
+{
+    public class DeviceErrorHistory : BaseEntity
+    {
+        public Guid DeviceId { get; set; }
+        public Guid ErrorId { get; set; }
+        public DateTime LastOccurredDate { get; set; }
+        public int OccurrenceCount { get; set; } // Số lần xảy ra
+        public string? Notes { get; set; }
+
+        // Navigation properties
+        public Device? Device { get; set; }
+        public Error? Error { get; set; }
+    }
+}
