e6fd96be54ec5489d3a0ce2e5dd88876fff2f4f1
dotrong-code <dotrong159357@gmail.com>
2025-05-21 16:58:36 +0700

    CRUD_Tasks

---
+++ BE/GRRWS.Infrastructure/DTOs/Task/CreateTaskDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRRWS.Infrastructure.DTOs.Task
+{
+    public class CreateTaskDTO
+    {
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string TaskType { get; set; }
+        public int? Priority { get; set; }
+        public string Status { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? ExpectedTime { get; set; }
+        public Guid AssigneeId { get; set; }
+        public DateTime? DeviceReturnTime { get; set; }
+        public string DeviceCondition { get; set; }
+        public string ReportNotes { get; set; }
+        public Guid CreatedBy { get; set; }
+        public List<Guid> ErrorIds { get; set; } // For linking to ErrorDetails
+        public List<Guid> SparepartIds { get; set; } // For linking to RepairSpareparts
+    }
+}
