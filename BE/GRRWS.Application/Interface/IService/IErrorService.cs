07bf86f14fdf878cfee0c6ca8a89c299aebb03d0
dotrong-code <dotrong159357@gmail.com>
2025-05-21 10:07:02 +0700

    updae

--- BE/GRRWS.Application/Interface/IService/IErrorService.cs
+++ BE/GRRWS.Application/Interface/IService/IErrorService.cs
@@ -1,9 +1,12 @@
 ﻿using GRRWS.Application.Common.Result;
+using GRRWS.Infrastructure.DTOs.RequestDTO;
 
 namespace GRRWS.Application.Interface.IService
 {
     public interface IErrorService
     {
         Task<Result> GetErrorSuggestionsAsync(string query, int maxResults);
+
+        Task<Result> GetRecommendedErrorsAsync(IssueIdsRequestDTO dto);
     }
 }
