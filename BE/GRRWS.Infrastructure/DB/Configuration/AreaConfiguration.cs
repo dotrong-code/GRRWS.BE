a6dbf61ce3e4a6b405f9ed0e86ac01ac1bc20c40
DuckAnt <nguduckant@gmail.com>
2025-05-23 21:38:18 +0700

    fix data

--- BE/GRRWS.Infrastructure/DB/Configuration/AreaConfiguration.cs
+++ BE/GRRWS.Infrastructure/DB/Configuration/AreaConfiguration.cs
@@ -17,21 +17,42 @@ namespace GRRWS.Infrastructure.DB.Configuration
                 new Area
                 {
                     Id = Guid.Parse("b1c2d3e4-0001-0001-0001-000000000001"),
-                    AreaName = "Main Production Floor",
+                    AreaName = "Khu Sản Xuất Chính A",
                     CreatedDate = DateTime.UtcNow,
                     ModifiedDate = DateTime.UtcNow
                 },
                 new Area
                 {
                     Id = Guid.Parse("b1c2d3e4-0002-0002-0002-000000000002"),
-                    AreaName = "Finishing Department",
+                    AreaName = "Khu Sản Xuất Chính B",
                     CreatedDate = DateTime.UtcNow,
                     ModifiedDate = DateTime.UtcNow
                 },
                 new Area
                 {
                     Id = Guid.Parse("b1c2d3e4-0003-0003-0003-000000000003"),
-                    AreaName = "Quality Control Area",
+                    AreaName = "Khu Kiểm Tra Chất Lượng",
+                    CreatedDate = DateTime.UtcNow,
+                    ModifiedDate = DateTime.UtcNow
+                },
+                new Area
+                {
+                    Id = Guid.Parse("b1c2d3e4-0004-0004-0004-000000000004"),
+                    AreaName = "Khu Cắt Vải",
+                    CreatedDate = DateTime.UtcNow,
+                    ModifiedDate = DateTime.UtcNow
+                },
+                new Area
+                {
+                    Id = Guid.Parse("b1c2d3e4-0005-0005-0005-000000000005"),
+                    AreaName = "Khu Thêu",
+                    CreatedDate = DateTime.UtcNow,
+                    ModifiedDate = DateTime.UtcNow
+                },
+                new Area
+                {
+                    Id = Guid.Parse("b1c2d3e4-0006-0006-0006-000000000006"),
+                    AreaName = "Khu Lưu Kho Thiết Bị",
                     CreatedDate = DateTime.UtcNow,
                     ModifiedDate = DateTime.UtcNow
                 }
