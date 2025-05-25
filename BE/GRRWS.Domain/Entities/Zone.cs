bb88f31c3b5e0ca194f3e9f408326bbe4e0d4be8
Thang <pqt2802@gmail.com>
2025-05-25 10:48:29 +0700

    fix

--- BE/GRRWS.Domain/Entities/Zone.cs
+++ BE/GRRWS.Domain/Entities/Zone.cs
@@ -3,7 +3,7 @@
     public class Zone : BaseEntity
     {
         public string? ZoneName { get; set; }
-        //public string? ZoneCode { get; set; } // e.g., A52-1, A52-2, etc.
+        public string? ZoneCode { get; set; } // e.g., A52-1, A52-2, etc.
         public Guid? AreaId { get; set; }
         public Area Area { get; set; }
         public ICollection<Position>? Positions { get; set; }
