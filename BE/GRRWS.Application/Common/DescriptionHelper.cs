eff41efda1414b86a2d5bb5a6f6ec4d320c7cf81
Thang <pqt2802@gmail.com>
2025-06-01 22:07:37 +0700

    add request des

---
+++ BE/GRRWS.Application/Common/DescriptionHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace GRRWS.Application.Common
+{
+    public static class DescriptionHelper
+    {
+        public static string GenerateRequestDescription(string deviceName)
+        {
+            if (string.IsNullOrWhiteSpace(deviceName))
+                throw new ArgumentException("DeviceName is required", nameof(deviceName));
+
+            return $"Yêu cầu kiểm tra máy {deviceName}";
+        }
+    }
+}
