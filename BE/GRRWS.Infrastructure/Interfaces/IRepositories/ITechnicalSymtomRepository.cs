ee18072f7ce45c2f4731abaeb090d4e200b7dc02
phanvongocphu <155410531+phanvongocphu@users.noreply.github.com>
2025-06-01 16:42:16 +0700

    Add CreateWarrantyReport, add new View for Report

---
+++ BE/GRRWS.Infrastructure/Interfaces/IRepositories/ITechnicalSymtomRepository.cs
@@ -0,0 +1,14 @@
+﻿using GRRWS.Infrastructure.DTOs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRRWS.Infrastructure.Interfaces.IRepositories
+{
+    public interface ITechnicalSymtomRepository
+    {
+        Task<List<SuggestObject>> GetNotFoundTechnicalSymtomDisplayNamesAsync(IEnumerable<Guid> technicalSymtomIds);
+    }
+}
