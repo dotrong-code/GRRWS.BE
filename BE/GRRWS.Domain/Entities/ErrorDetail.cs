967500d6069f11a6e3ccb8b2bc881407f10dbbee
Thang <pqt2802@gmail.com>
2025-06-07 13:13:26 +0700

    add

--- BE/GRRWS.Domain/Entities/ErrorDetail.cs
+++ BE/GRRWS.Domain/Entities/ErrorDetail.cs
@@ -4,9 +4,13 @@
     {
         public Guid ReportId { get; set; }
         public Guid ErrorId { get; set; }
+        public Guid? ErrorGuideLineId { get; set; }
         public Guid? TaskId { get; set; }
         public Report Report { get; set; }
         public Error Error { get; set; }
         public Tasks Task { get; set; }
+        public ErrorGuideline? ErrorGuideline { get; set; } // Nullable to allow for cases without a guideline
+        public ICollection<ErrorFixProgress>? ProgressRecords { get; set; }
+        public ICollection<SparePartUsage>? SparePartUsages { get; set; } // Collection of spare parts used for this error
     }
 }
